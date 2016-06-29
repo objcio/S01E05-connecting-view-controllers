6783cb9373238f5a4aa80820e3b20aef8e1e2fb7
Florian Kugler <mail@floriankugler.com>
2016-06-29 17:29:35 +0200

    Final

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -14,34 +14,10 @@ struct Episode {
 
 class ProfileViewController: UIViewController {
     var person: String = ""
-}
+    var didTapClose: () -> () = {}
 
-class EpisodesViewController: UITableViewController {
-    let episodes = [Episode(title: "Episode One"), Episode(title: "Episode Two"), Episode(title: "Episode Three")]
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return episodes.count
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        let episode = episodes[indexPath.row]
-        cell.textLabel?.text = episode.title
-        return cell
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if let vc = segue.destinationViewController as? DetailViewController {
-            vc.episode = episodes[tableView.indexPathForSelectedRow!.row]
-        } else if let nc = segue.destinationViewController as? UINavigationController,
-            pvc = nc.viewControllers.first as? ProfileViewController {
-            pvc.person = "My Name"
-        } else {
-            fatalError()
-        }
-    }
-    
-    @IBAction func unwindToHere(segue: UIStoryboardSegue) {
+    @IBAction func close(sender: AnyObject) {
+        didTapClose()
     }
 }
 
@@ -54,16 +30,71 @@ class DetailViewController: UIViewController {
     var episode: Episode?
 }
 
+class EpisodesViewController: UITableViewController {
+    let episodes = [Episode(title: "Episode One"), Episode(title: "Episode Two"), Episode(title: "Episode Three")]
+    var didSelect: (Episode) -> () = { _ in }
+    var didTapProfile: () -> () = {}
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let episode = episodes[indexPath.row]
+        didSelect(episode)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let episode = episodes[indexPath.row]
+        cell.textLabel?.text = episode.title
+        return cell
+    }
+    
+    @IBAction func showProfile(sender: AnyObject) {
+        didTapProfile()
+    }
+}
+
+final class App {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let navigationController: UINavigationController
+
+    init(window: UIWindow) {
+        navigationController = window.rootViewController as! UINavigationController
+        let episodesVC = navigationController.viewControllers[0] as! EpisodesViewController
+        episodesVC.didSelect = { _ in self.showProfile() }
+        episodesVC.didTapProfile = showProfile
+    }
+    
+    func showEpisode(episode: Episode) {
+        let detailVC = storyboard.instantiateViewControllerWithIdentifier("Detail") as! DetailViewController
+        detailVC.episode = episode
+        navigationController.pushViewController(detailVC, animated: true)
+    }
+    
+    func showProfile() {
+        let profileNC = self.storyboard.instantiateViewControllerWithIdentifier("Profile") as! UINavigationController
+        let profileVC = profileNC.viewControllers[0] as! ProfileViewController
+        profileVC.didTapClose = {
+            self.navigationController.dismissViewControllerAnimated(true, completion: nil)
+        }
+        navigationController.presentViewController(profileNC, animated: true, completion: nil)
+    }
+}
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var app: App?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if let window = window {
+            app = App(window: window)
+        }
         return true
     }
 
